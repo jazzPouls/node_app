53d6700f8271e5888037d12400346c191be41460
jazzPouls <jazzpouls@icloud.com>
2021-01-16 13:22:33 -0800

    502

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("ran new code")
+    res.send("GETtING RId OF 502")
 });
 
 const port = process.env.port || 3000;
+// const port = 3001;
 app.listen(port, () => {
-    console.log("testsetqweoirqweproiq")
+    console.log("geEETIINNG RID OF 502 ERROR")
 });
