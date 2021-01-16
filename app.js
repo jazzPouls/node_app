dba009b5a4ee75803cc514055d4637ba0b63c7e4
jazzPouls <jazzpouls@icloud.com>
2021-01-16 13:51:06 -0800

    8080 port

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => {
 
 const port = process.env.port || 3000;
 // const port = 3001;
+// const port = 8081;
 app.listen(port, () => {
     console.log("port used is : " + port)
     console.log("geEETIINNG RID OF 502 ERROR")
