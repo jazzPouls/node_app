7b446760d483ac4163acb36b4a373bef5858db1e
jazzPouls <jazzpouls@icloud.com>
2021-01-16 11:49:47 -0800

    start'

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("WEOMAOEMFWE toteh qwhome epabge")
+});
+
+app.listen(3000, () => {
+    console.log("RUNNNAISDFNSIDFNANG")
+});
