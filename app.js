24cdc678202d333b0906c87589989132145b0afd
jazzPouls <jazzpouls@icloud.com>
2021-01-16 13:52:13 -0800

    semicolon

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ const port = process.env.port || 3000;
 // const port = 3001;
 // const port = 8081;
 app.listen(port, () => {
-    console.log("port used is : " + port)
-    console.log("geEETIINNG RID OF 502 ERROR")
+    console.log("port used is : " + port);
+    console.log("geEETIINNG RID OF 502 ERROR");
 });
