0253ec04505a898a5bcda1586c744e8fe3e4819e
jazzPouls <jazzpouls@icloud.com>
2021-01-26 12:13:05 -0700

    mongodb start

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("got rid fof it 555")
+    res.send("started mongodbPROJ")
 });
 
 const port = process.env.port || 3000;
@@ -10,5 +10,5 @@ const port = process.env.port || 3000;
 // const port = 8081;
 app.listen(port, () => {
     console.log("port used is : " + port);
-    console.log("GOT RIDIF OEF ITTTTTTt ");
+    console.log("Starting mongodb proj ");
 });
