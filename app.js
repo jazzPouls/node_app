6dffca3c4e7c747e32875c46d3db28dbb5971d04
jazzPouls <jazzpouls@icloud.com>
2021-01-31 09:40:56 -0800

    check api/users

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("started mongodbPROJ")
+    res.send("started mongodbPROJ NDOMEODN")
+});
+
+app.get('/api/users', (req, res) => {
+    res.send("api called");
 });
 
 const port = process.env.port || 3000;
-// const port = 3001;
-// const port = 8081;
+
+// AWS EB port = 8080;
 app.listen(port, () => {
     console.log("port used is : " + port);
-    console.log("Starting mongodb proj ");
+    console.log("Starting mongodb proj NOEMONED");
 });
